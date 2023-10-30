a0ab630a5f1b541593be0b905be5fcd45a7d219c
sumit_pro-cs <sumit.mandal@pro-cs.in>
2023-10-30 16:16:56 +0530

    ch6

--- Controllers/InsiderTrading/ReminderSetUpController.cs
+++ Controllers/InsiderTrading/ReminderSetUpController.cs
@@ -474,5 +474,10 @@ namespace ProcsDLL.Controllers.InsiderTrading
             remResponce = remRequest.ReminderSave();
             return remResponce;
         }
+
+        public void Display()
+        {
+            //Console.WriteLine("HI");
+        }
     }
 }
